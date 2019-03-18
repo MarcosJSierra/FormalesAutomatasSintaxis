c7334f014c9f83154670e212a6e9e5bf1fd0b6d2
msunder <sierramarcos146@gmail.com>
2019-03-18 00:02:21 -0600

    cambios main

--- AlgoritmoAutomata.rb
+++ AlgoritmoAutomata.rb
@@ -1,10 +1,13 @@
 #Este es el algoritmo dedicado a la revision sintaxica del proyectos 
 class Analizador
-	attr_accessor :sumas, :linea, :lineal, :caracter, :ultima_palabra, :palabra
+	attr_accessor :sumas, :linea, :lineal, :caracter, :ultima_palabra, :palabra, :error, :nlinea, :nlinea_error
+	attr_accessor :cantidades
 	def initialize()
+		self.cantidades=Array.new(27)
 		self.sumas = 0
 		self.lineal = 0
-		#linea = "hola"
+		self.error = false
+		
 	end
 	def aumentar
 		self.sumas = self.sumas + 1
@@ -16,8 +19,71 @@ class Analizador
 		contador = 0
 
 		while (contador < lineal)
-			
-			puts linea[contador]
+			caracter = linea[contador]
+			case caracter
+			#Inicio Letras
+			when "a"
+				puts "caracter a"
+			when "b"
+			when "c"
+			when "d"
+			when "e"		
+			when "f"
+			when "g"
+			when "h"
+			when "i"
+			when "j"
+			when "k"
+			when "l"
+			when "m"
+			when "n"
+			when "o"
+				puts "caracter o"
+			when "p"
+			when "q"
+			when "r"
+			when "s"
+			when "t"
+			when "u"
+			when "v"
+			when "w"		
+			when "x"
+			when "y"
+			when "z"
+			#inicio Digitos
+			when "1"
+			when "2"
+			when "3"
+			when "4"
+			when "5"
+			when "6"
+			when "7"
+			when "8"
+			when "9"
+			when "0"
+			#inicio operadores 
+			when "+"
+				if(palabra.length == 0)
+					self.cantidades[10]
+				else
+				end
+			when "-"
+			when "*"
+			when "/"		
+			when "%"
+			when "="
+			when "<"
+			when ">"
+			when ")"
+			when "("
+			when "{"
+			when "}"
+			when ";"
+			when " "
+			else 
+				error =true
+			end
+
 			contador += 1
 		end
 	end
